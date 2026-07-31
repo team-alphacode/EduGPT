70857f5fac7ffe7fd5d40ff8269893a58b0a0552
JosueCastiS <josue.sibriancasti0814@gmail.com>
2026-07-31 09:43:13 -0600

    feat: find books

--- src/edugpt/model.py
+++ src/edugpt/model.py
@@ -1,13 +1,25 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
 class EduGPT:
     def __init__(self):
         self.name = "EduGPT"
-        self.corpus = []
+        self.version = "0.0.2"
+        self.corpus = ""
         self.vocabulary = set()
         self.total_words = 0
+        
+        # Carpeta donde estarán todos los libros
+        self.books_path = BASE_DIR / "data" / "books"
+        # Carpeta donde guardaremos el corpus unificado
+        self.processed_path = BASE_DIR / "data" / "processed"
+        # Archivo del corpus
+        self.corpus_path = self.processed_path / "corpus.txt"
 
     def show_information(self):
         print("=" * 45)
-        print(f"{self.name} v0.0.1")
+        print(f"        {self.name} v{self.version}")
         print("=" * 45)
         print()
         print(f"Modelo: {self.name}")
@@ -18,3 +30,21 @@ class EduGPT:
         print("EduGPT:")
         print("Hola, todavía no sé nada.")
         print("Enséñame un libro.")
+
+    def find_books(self):
+        """
+        Busca todos los archivos de texto
+        dentro de la carpeta books.
+        """
+        books = list(self.books_path.glob("*.txt"))
+        return books
+
+    def show_books(self):
+        books = self.find_books()
+        print("\n========== LIBROS ENCONTRADOS ==========\n")
+        if not books:
+            print("No se encontró ningún libro.")
+            return
+        for i, book in enumerate(books, 1):
+            print(f"{i}. {book.name}")
+        print(f"\nTotal de libros: {len(books)}")
